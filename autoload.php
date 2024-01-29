ba77f5a239e31b38763e386865dcb71095587f4f
Helios Tech <155942467+heliostechlabs@users.noreply.github.com>
2024-01-29 15:28:24 +0545

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/home/ec2-user/axis-api/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 // If you have any additional setup or configuration, you can include it here.
 
